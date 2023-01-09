28cfe0e2a3f438eb4d763e4f82376321ca684255
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-01-10 02:49:43 +0530

    Added home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import './pages/Home/Home-page.css';
+import './pages/Home/Home-page';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Index from './pages/Home/index';
 
 function App() {
   return (
     <>
-    <h1>Hello world</h1>
+    <Index/>
     </>
   )
 }
