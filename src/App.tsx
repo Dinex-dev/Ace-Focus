14e8fdd23d3c5c5f137460f5cafde5a46e06b985
Kanishk <rawat27kanishk@gmail.com>
2023-01-10 19:26:49 +0530

    Set Profile in App tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
-import Index from './pages/Home/index';
 
 function App() {
   return (
     <>
     <Profile />
-    <Index/>
     </>
   )
 }
