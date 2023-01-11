a8b20116d8e4047f5eada1b48a87ccb9d7cd2331
Pushkar <bansalpushkar100@gmail.com>
2023-01-11 23:20:09 +0530

    users page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import "./App.css";
+import Whatsapp from './assets/img/WhatsApp.png';
+import Github from './assets/img/github.png';
+import Gmail from './assets/img/gmail.png';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Data from './assets/data'
 
-function App() {
+const App = (props:Object) => {
   return (
-    <>
-    <h1>Hello world</h1>
-    </>
+    <div className="pushkar-container">
+      <h2 className="heading">Team Name</h2>
+      <div className="users-desktop">
+        {Data.map((user,index)=>{
+          return <div key={index} className="card user shadow-sm">
+          <div className="my-image-container">
+            <img src={user.profilePic} height="150px" alt="..."/>
+          </div>
+            <h5 className="card-title m-3">{user.name}</h5>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item fw-bold">{user.primaryTask}</li>
+            <li className="list-group-item">{user.secondaryTask}</li>
+          </ul>
+          <div className="card-body">
+            <a href={user.whatsapp} className="card-link"><img src={Whatsapp} height="20px" /></a>
+            <a href={user.gmail} className="card-link"><img src={Gmail} height="20px" /></a>
+            <a href={user.github} className="card-link"><img src={Github} height="20px" /></a>
+          </div>
+        </div>
+        })}
+      </div>
+
+      <div className="display-mobile">
+        
+      </div>
+    </div>
   )
 }
 
---
+++ src/assets/data.tsx
@@ -0,0 +1,85 @@
+const data = [
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+    {
+        name:"Pushkar",
+        profilePic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+        primaryTask:"Working on Ace Focus",
+        secondaryTask:"Chilling!!",
+        whatsapp:"#",
+        gmail:"bansalpushkar100@gmail.com",
+        github:"https://www.github.com/pushkar707"
+    },
+]
+
+export default data
