96c0f319c60e6cd7d572513d63a8ed0d7ba60120
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-01-12 18:18:51 +0530

    Added props to home file

---
+++ src/pages/Home/Home-page-constants.tsx
@@ -0,0 +1,22 @@
+export const domains = [{
+    name : "Web Development"
+},
+{
+    name : "Programming"
+},
+{
+    name : "Cyber Security"
+},
+{
+    name : "Designing"
+},
+{
+    name : "App Development"
+},
+{
+    name : "Content and Marketing"
+},
+{
+    name : "Research and Development"
+}
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,21 @@
 import React from 'react'
 import './Home-page.css';
+import {domains} from './Home-page-constants';
 
 export default function Index() {
   return (
-    <div>
+    <>
          {/* Home page domain boxes container */}
         <div className="HomePage-container">
-             {/* Home page domain box  */}
+          {domains.map((items:any)=>(
+            <> 
+            {/* Home page domain box  */}
             <div className="HomePage-Box">
-                <h2 >Web Development</h2>
+                <h2>{items.name}</h2>
             </div>
-            <div className="HomePage-Box">
-                <h2>Programming</h2>
-            </div>
-            <div className="HomePage-Box">
-                <h2>Cyber Security</h2>
-            </div>
-            <div className="HomePage-Box">
-                <h2>Designing</h2>
-            </div>
-            <div className="HomePage-Box">
-                <h2>App Development</h2>
-            </div>
-            <div className="HomePage-Box">
-                <h2>Content and Marketing</h2>
-            </div>
-            <div className="HomePage-Box">
-                <h2>Research and Development</h2>
-            </div>
+            </>
+          ))}
         </div>
-    </div>
+    </>
   )
 }
